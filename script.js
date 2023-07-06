c356c6b4a3e2c93c96832f5b8fac2393d21eb6d8
mack69 <ghoshmayukh.janai@gmail.com>
2023-07-07 01:18:39 +0530

    Temp Reading

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const apiKey = "ea0df21bb575b6d49d32dd952fcdba9b";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?&appid=ea0df21bb575b6d49d32dd952fcdba9b&units=metric&q=kolkata";
 
-async function weatherCheck(city) {
-  const response = await fetch(apiUrl + city + "&appid=${apiKey}");
+async function weatherCheck() {
+  const response = await fetch(apiUrl + "&appid=${apiKey}");
   var data = await response.json();
 
   console.log(data);
@@ -11,7 +11,6 @@ async function weatherCheck(city) {
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.wind.speed = "km/h";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 }
-
 weatherCheck();
