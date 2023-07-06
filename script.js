c5e01b5bc770622a915ad28714ed43cbccfc8aeb
mack69 <ghoshmayukh.janai@gmail.com>
2023-07-07 01:31:48 +0530

    All city Temp

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const apiKey = "ea0df21bb575b6d49d32dd952fcdba9b";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?&appid=ea0df21bb575b6d49d32dd952fcdba9b&units=metric&q=kolkata";
+  "https://api.openweathermap.org/data/2.5/weather?&appid=ea0df21bb575b6d49d32dd952fcdba9b&units=metric&q=";
 
-async function weatherCheck() {
-  const response = await fetch(apiUrl + "&appid=${apiKey}");
+async function weatherCheck(city) {
+  const response = await fetch(apiUrl + city + "&appid=${apiKey}");
   var data = await response.json();
 
-  console.log(data);
+  const searchBox = document.querySelector(".search input");
+  const searchBtn = document.querySelector(".search input");
 
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 }
-weatherCheck();
+
+searchBtn.addEventListener("click", () => {
+  weatherCheck(searchBox.value);
+});
